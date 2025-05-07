c8c8dd01540b667e57312493b9cee22811c5bdbf
Danish <bhandaridanish96@gmail.com>
2025-05-07 19:29:29 +0530

    custom user registeration added

--- chaiheadq/tweet/forms.py
+++ chaiheadq/tweet/forms.py
@@ -1,8 +1,16 @@
 from django import forms
 from .models import Tweet
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User  
 
 class TweetForms(forms.ModelForm):
     # this meta class is necessary
     class Meta:
         model=Tweet
         fields=['text','photo']
+
+class UserRegisterationForm(UserCreationForm):
+    email=forms.EmailField()
+    class Meta:
+        model=User
+        fields=('username','email','password1','password2') # here built in table is used thats why we used tuple
--- chaiheadq/tweet/views.py
+++ chaiheadq/tweet/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from .models import Tweet
 from .forms import TweetForms
 from django.shortcuts import get_object_or_404,redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+from .forms import UserRegisterationForm
 # Create your views here.
 
 def index(request):
@@ -13,6 +16,7 @@ def tweet_list(request):
     return render(request,'tweet_list.html',{'tweets':tweets})
 
 # creating tweets
+@login_required # with this this route is protected now
 def tweet_create(request):
     if request.method=='POST':
         form=TweetForms(request.POST,request.FILES)  # by second arg we can also accept files
@@ -28,6 +32,7 @@ def tweet_create(request):
     return render(request,'tweet_form.html',{'form':form})
 
 # editing tweet forms
+@login_required
 def tweet_edit(request,tweet_id):
     tweet=get_object_or_404(Tweet,pk=tweet_id,user=request.user) # konse models ke andar tweet dekhu - 1st param
     if request.method=='POST':
@@ -41,9 +46,23 @@ def tweet_edit(request,tweet_id):
         form=TweetForms(instance=tweet)
     return render(request,'tweet_form.html',{'form':form})
 
+@login_required
 def tweet_delete(request,tweet_id):
     tweet=get_object_or_404(Tweet,pk=tweet_id,user=request.user)
     if request.method=='POST':
         tweet.delete()
         return redirect('tweet_list')
     return render(request,'tweet_confirm_delete.html',{'tweet':tweet})
+
+def register(request):
+    if request.method=='POST':
+        form=UserRegisterationForm(request.POST)
+        if form.is_valid():
+            user=form.save(commit=False)
+            user.set_password(form.cleaned_data['password1'])
+            user.save()
+            login(request,user)  # hence login functionality is handled by django 
+            return redirect('tweet_list')
+    else:
+        form=UserRegisterationForm()
+    return render(request,'registration/register.html',{'form':form})
